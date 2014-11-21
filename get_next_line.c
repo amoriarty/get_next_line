e73d89c7c631b28eaa43f91a055619319122f83b
alegent <alegent@student.42.fr>
2014-11-21 15:06:46 +0100

    With different BUFF_SIZE

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/21 13:51:06 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/21 14:00:15 by alegent          ###   ########.fr       */
+/*   Updated: 2014/11/21 15:02:32 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,27 @@
 int				get_next_line(const int fd, char **line)
 {
 	int				ret;
-	char			buffer[BUFF_SIZE];
+	char			buffer[BUFF_SIZE + 1];
 	char			*tmp;
-	static char		*too_much;
+	static char		*too;
 
 	if (fd != -1 || !line)
 	{
-		if (too_much && (tmp = ft_strchr(too_much, EOL)))
+		if (too && (tmp = ft_strchr(too, EOL)))
 		{
-			*line = ft_strsub(too_much, 0, ft_strlen(too_much) - ft_strlen(tmp));
-			too_much = ft_strchr(too_much, EOL) + 1;
+			*line = ft_strsub(too, 0, ft_strlen(too) - ft_strlen(tmp));
+			too = ft_strchr(too, EOL) + 1;
 			return (1);
 		}
-		while ((ret = read(fd, buffer, BUFF_SIZE + 1)))
+		while ((ret = read(fd, buffer, BUFF_SIZE)))
 		{
 			buffer[ret] = '\0';
-			too_much = ft_strjoin(too_much, buffer);
-			if ((tmp = ft_strchr(too_much, EOL)))
+			too = ft_strjoin(too, buffer);
+			if ((tmp = ft_strchr(too, EOL)))
 			{
-				*line = ft_strsub(too_much, 0, ft_strlen(too_much) - ft_strlen(tmp));
-				too_much = ft_strchr(too_much, EOL) + 1;
+				*line = ft_strsub(too, 0, ft_strlen(too) - ft_strlen(tmp));
+				too = ft_strchr(too, EOL) + 1;
+				ft_strdel(&tmp);
 				return (1);
 			}
 		}
