33bd2208db72c992382d78c2bcbbc6621f84a8e1
alegent <alegent@student.42.fr>
2014-11-24 15:16:52 +0100

    a la norme mais fuite de memoire

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,24 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/21 13:51:06 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/21 15:02:32 by alegent          ###   ########.fr       */
+/*   Updated: 2014/11/24 15:16:07 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 #include "get_next_line.h"
+
+static int		ft_static(char ***line, char **too, char **tmp)
+{
+	if (*too && (*tmp = ft_strchr(*too, EOL)))
+	{
+		**line = ft_strsub(*too, 0, ft_strlen(*too) - ft_strlen(*tmp));
+		*too = ft_strchr(*too, EOL) + 1;
+		return (TRUE);
+	}
+	return (FALSE);
+}
 
 int				get_next_line(const int fd, char **line)
 {
@@ -23,12 +34,8 @@ int				get_next_line(const int fd, char **line)
 
 	if (fd != -1 || !line)
 	{
-		if (too && (tmp = ft_strchr(too, EOL)))
-		{
-			*line = ft_strsub(too, 0, ft_strlen(too) - ft_strlen(tmp));
-			too = ft_strchr(too, EOL) + 1;
+		if (ft_static(&line, &too, &tmp) == TRUE)
 			return (1);
-		}
 		while ((ret = read(fd, buffer, BUFF_SIZE)))
 		{
 			buffer[ret] = '\0';
@@ -37,7 +44,6 @@ int				get_next_line(const int fd, char **line)
 			{
 				*line = ft_strsub(too, 0, ft_strlen(too) - ft_strlen(tmp));
 				too = ft_strchr(too, EOL) + 1;
-				ft_strdel(&tmp);
 				return (1);
 			}
 		}
